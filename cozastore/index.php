9782573481766c61b1e418499da949ec002480af
Assasinator-ops <68000010+Assasinator-ops@users.noreply.github.com>
2021-12-08 05:52:10 +0300

    commit

---
+++ cozastore/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+	require_once("partials/dbconn.php");
+	include("partials/head.php");
+?>
+<body class="animsition">
+	<?php 
+		include("partials/header.php"); 
+		include("partials/slider.php");
+		include("partials/banner.php"); 
+		include("partials/footer.php");
+	?>
+</body>
+</html>
